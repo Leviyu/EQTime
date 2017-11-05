d7c0ba1d5ac6bc338cc916c2b55b616f7dd9d06c
Leviyu <hongyu.hot@icloud.com>
2017-11-04 17:28:52 -0700

    clean

--- SRC/big_run.sh
+++ SRC/big_run.sh
@@ -1,15 +1,21 @@
 #!/bin/tcsh
 
+
+set PWD = `pwd`
+set INFILE = $PWD/INFILE.mother
+set BIG_ID = `cat $INFILE|grep TASK_NAME |awk 'NR==1 {print $2}'`
+set EQ_LIST = `cat $INFILE |grep EQ_LIST|awk 'NR==1 {print $2}'`
+set processes = `cat $INFILE |grep Process_Number|awk 'NR==1 {print $2}'`
+
+
+
+
+
+
+
+set EQ_LIST = $PWD/$EQ_LIST
+cp $EQ_LIST $PWD/INPUT_EQ_LIST
 set TPWD = `pwd`
-
-#sleep 5h
-# Ed_checked_2004_not_processed
-# this is the 45 EQ that is the left over of 
-# part1 and part2 for 2004 onward patch
-set BIG_ID = Patch11
-set EQ_LIST = $TPWD/EQ_LIST.PATCH11
-
-set processes = 4
 
 set EQ_NUM = `cat $EQ_LIST |wc -l`
 set INC = `echo "$EQ_NUM / $processes "|bc -l`
@@ -22,14 +28,14 @@ echo "--> Working on $PART / $processes for $BIG_ID"
 
 set BEG = `echo " ($PART - 1)*$INC +1"|bc`
 set END = `echo "($PART)*$INC "|bc`
-set PART_FILE = $TPWD/list.EQ.${BIG_ID}.${PART}
+set PART_FILE = $TPWD/LOG/list.EQ.${BIG_ID}.${PART}
 cat $EQ_LIST |awk '{ if(NR>='$BEG' && NR<='$END') print $0}' >! $PART_FILE
 ##cat $PART_FILE
 cd $TPWD
-set ID = ${BIG_ID}_ID_${PART}
+set ID = ${BIG_ID}_${PART}
 cp $PART_FILE $TPWD/INPUT_EQ_LIST
-csh $TPWD/mother.sh $ID
-sleep 5s
+csh $TPWD/mother.sh $ID 
+sleep 3s
 
 rm $TPWD/INPUT_EQ_LIST
 @ PART ++
